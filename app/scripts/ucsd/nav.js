f4c62b980e886405408d9e4277cf96c6d2282fb2
Jonathan Chang <jdkschang@gmail.com>
2015-02-25 13:24:50 -0800

    finished the page transition when button is pressed

--- app/scripts/ucsd/nav.js
+++ app/scripts/ucsd/nav.js
@@ -3,7 +3,8 @@
         var navBtn              = document.querySelector('.nav-btn');
         var navList             = document.querySelector('.navdrawer-container');
         var layoutHeader        = document.querySelector('.layout-header');         // for menu button transition
-        var layoutBody        = document.querySelector('.layout-body');
+        var layoutMain          = document.querySelector('.layout-main');
+        var layoutFooter        = document.querySelector('.layout-footer');
         var navIsOpenedClass    = 'navbar-is-opened';
         var menuOpen            = 'open';
         var navListIsOpened     = false;
@@ -13,11 +14,17 @@
 
             if (!navListIsOpened) {
                 addClass(navList, navIsOpenedClass);
-                addClass(layoutBody, menuOpen);
+
+                addClass(layoutHeader, menuOpen);
+                addClass(layoutMain, menuOpen);
+                addClass(layoutFooter, menuOpen);
                 navListIsOpened = true;
             } else {
                 removeClass(navList, navIsOpenedClass);
-                removeClass(layoutBody, menuOpen);
+
+                removeClass(layoutHeader, menuOpen);
+                removeClass(layoutMain, menuOpen);
+                removeClass(layoutFooter, menuOpen);
                 navListIsOpened = false;
             }
         });
