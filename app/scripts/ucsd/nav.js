aa7cde1064f51f58e0f374e2fa4f150309c5aa45
Jonathan Chang <jdkschang@gmail.com>
2015-03-23 08:52:56 -0700

    added possible workaround for ie7 search icon support and more alert statements in nav.js

--- app/scripts/ucsd/nav.js
+++ app/scripts/ucsd/nav.js
@@ -1,109 +1,49 @@
 (function(document) {
-    var getElementsByClassName = function (className, tag, elm){
-        if (document.getElementsByClassName) {
-            getElementsByClassName = function (className, tag, elm) {
-                elm = elm || document;
-                var elements = elm.getElementsByClassName(className),
-                    nodeName = (tag)? new RegExp("\\b" + tag + "\\b", "i") : null,
-                    returnElements = [],
-                    current;
-                for(var i=0, il=elements.length; i<il; i+=1){
-                    current = elements[i];
-                    if(!nodeName || nodeName.test(current.nodeName)) {
-                        returnElements.push(current);
-                    }
-                }
-                return returnElements;
-            };
-        }
-        else if (document.evaluate) {
-            getElementsByClassName = function (className, tag, elm) {
-                tag = tag || "*";
-                elm = elm || document;
-                var classes = className.split(" "),
-                    classesToCheck = "",
-                    xhtmlNamespace = "http://www.w3.org/1999/xhtml",
-                    namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace)? xhtmlNamespace : null,
-                    returnElements = [],
-                    elements,
-                    node;
-                for(var j=0, jl=classes.length; j<jl; j+=1){
-                    classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
-                }
-                try	{
-                    elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
-                }
-                catch (e) {
-                    elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
-                }
-                while ((node = elements.iterateNext())) {
-                    returnElements.push(node);
-                }
-                return returnElements;
-            };
-        }
-        else {
-            getElementsByClassName = function (className, tag, elm) {
-                tag = tag || "*";
-                elm = elm || document;
-                var classes = className.split(" "),
-                    classesToCheck = [],
-                    elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag),
-                    current,
-                    returnElements = [],
-                    match;
-                for(var k=0, kl=classes.length; k<kl; k+=1){
-                    classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
-                }
-                for(var l=0, ll=elements.length; l<ll; l+=1){
-                    current = elements[l];
-                    match = false;
-                    for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
-                        match = classesToCheck[m].test(current.className);
-                        if (!match) {
-                            break;
-                        }
-                    }
-                    if (match) {
-                        returnElements.push(current);
-                    }
-                }
-                return returnElements;
-            };
-        }
-        return getElementsByClassName(className, tag, elm);
-    };
-        if (!document.querySelectorAll) {
-            document.querySelectorAll = function (selectors) {
-                var style = document.createElement('style'), elements = [], element;
-                document.documentElement.firstChild.appendChild(style);
-                document._qsa = [];
+    // Document.querySelectorAll method
+    // http://ajaxian.com/archives/creating-a-queryselector-for-ie-that-runs-at-native-speed
+    // Needed for: IE7-
+    if (!document.querySelectorAll) {
+        document.querySelectorAll = function(selectors) {
+            var style = document.createElement('style'), elements = [], element;
+            document.documentElement.firstChild.appendChild(style);
+            document._qsa = [];
 
-                style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-                window.scrollBy(0, 0);
-                style.parentNode.removeChild(style);
+            style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+            window.scrollBy(0, 0);
+            style.parentNode.removeChild(style);
 
-                while (document._qsa.length) {
-                    element = document._qsa.shift();
-                    element.style.removeAttribute('x-qsa');
-                    elements.push(element);
-                }
-                document._qsa = null;
-                return elements;
-            };
-        }
+            while (document._qsa.length) {
+                element = document._qsa.shift();
+                element.style.removeAttribute('x-qsa');
+                elements.push(element);
+            }
+            document._qsa = null;
+            return elements;
+        };
+    }
 
-        if (!document.querySelector) {
-            document.querySelector = function (selectors) {
-                var elements = document.querySelectorAll(selectors);
-                return (elements.length) ? elements[0] : null;
-            };
-        }
+    // Document.querySelector method
+    // Needed for: IE7-
+    if (!document.querySelector) {
+        document.querySelector = function(selectors) {
+            var elements = document.querySelectorAll(selectors);
+            return (elements.length) ? elements[0] : null;
+        };
+    }
+
+    // Document.getElementsByClassName method
+    // Needed for: IE8-
+    if (!document.getElementsByClassName) {
+        document.getElementsByClassName = function(classNames) {
+            classNames = String(classNames).replace(/^|\s+/g, '.');
+            return document.querySelectorAll(classNames);
+        };
+    }
 
     var mainNav = function() {
-        alert('before navBtn : removed . maybe style display will work now');
+        alert('added separate support for qsa, and gebcn');
         var navBtn              = document.querySelectorAll('btn-nav');
-        if(!navBtn.querySelectorAll) navBtn.style.display = "block";
+        if(!navBtn.querySelectorAll) alertA('navBtn qsa NULL');
         var navList             = document.getElementsByClassName('navdrawer-container');
         var layoutHeader        = document.getElementsByClassName('layout-header');         // for menu button transition
         var layoutMain          = document.getElementsByClassName('layout-main');
