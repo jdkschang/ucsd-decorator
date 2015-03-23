f764f20548f33f67b8d7e258942affb0abbd1936
Jonathan Chang <jdkschang@gmail.com>
2015-03-23 01:25:35 -0700

    3-5 tries later, finally got a polyfill to work for getElementsByClassName ie7

--- app/scripts/ucsd/nav.js
+++ app/scripts/ucsd/nav.js
@@ -1,39 +1,38 @@
 (function(document) {
-    var polyfillQuery = function() {
-        if (!document.querySelectorAll) {
-            document.querySelectorAll = function (selectors) {
-                var style = document.createElement('style'), elements = [], element;
-                document.documentElement.firstChild.appendChild(style);
-                document._qsa = [];
-
-                style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-                window.scrollBy(0, 0);
-                style.parentNode.removeChild(style);
-
-                while (document._qsa.length) {
-                    element = document._qsa.shift();
-                    element.style.removeAttribute('x-qsa');
-                    elements.push(element);
+    if (!document.getElementsByClassName) {
+        document.getElementsByClassName = function(search) {
+            var d = document, elements, pattern, i, results = [];
+            if (d.querySelectorAll) { // IE8
+                return d.querySelectorAll("." + search);
+            }
+            if (d.evaluate) { // IE6, IE7
+                pattern = ".//*[contains(concat(' ', @class, ' '), ' " + search + " ')]";
+                elements = d.evaluate(pattern, d, null, 0, null);
+                while ((i = elements.iterateNext())) {
+                    results.push(i);
                 }
-                document._qsa = null;
-                return elements;
-            };
+            } else {
+                elements = d.getElementsByTagName("*");
+                pattern = new RegExp("(^|\\s)" + search + "(\\s|$)");
+                for (i = 0; i < elements.length; i++) {
+                    if ( pattern.test(elements[i].className) ) {
+                        results.push(elements[i]);
+                    }
+                }
+            }
+            return results;
         }
-
-        if (!document.querySelector) {
-            document.querySelector = function (selectors) {
-                var elements = document.querySelectorAll(selectors);
-                return (elements.length) ? elements[0] : null;
-            };
-        }
-    };
+    }
 
     var mainNav = function() {
-        var navBtn              = document.querySelector('.btn-nav');
-        var navList             = document.querySelector('.navdrawer-container');
-        var layoutHeader        = document.querySelector('.layout-header');         // for menu button transition
-        var layoutMain          = document.querySelector('.layout-main');
-        var layoutFooter        = document.querySelector('.layout-footer');
+        alert('pre navBtn: 3.4 removed few lines');
+        var navBtn              = document.getElementsByClassName('btn-nav');
+        alert('navBtn processed');
+        var navList             = document.getElementsByClassName('navdrawer-container');
+        alert('navList processed');
+        var layoutHeader        = document.getElementsByClassName('.layout-header');         // for menu button transition
+        var layoutMain          = document.getElementsByClassName('.layout-main');
+        var layoutFooter        = document.getElementsByClassName('.layout-footer');
         var navIsOpenedClass    = 'navbar-is-opened';
         var menuOpen            = 'open';
         var navListIsOpened     = false;
@@ -73,8 +72,8 @@
     };
 
     var mainSubNav = function() {
-        var subNav          = document.querySelector('.navbar-subnav');
-        var subList         = document.querySelector('.navbar-sublist');
+        var subNav          = document.getElementsByClassName('.navbar-subnav');
+        var subList         = document.getElementsByClassName('.navbar-sublist');
         var subNavList      = 'subnav-is-opened';
         var subNavHover     = 'subnav-hover';
         var subNavIsOpened  = false;
@@ -130,8 +129,8 @@
     };
 
     var mainSearch = function() {
-        var searchBtn = document.querySelector('.search-toggle');
-        var searchContent = document.querySelector('.search-content');
+        var searchBtn = document.getElementsByClassName('.search-toggle');
+        var searchContent = document.getElementsByClassName('.search-content');
         var searchOpen = 'search-is-open';
         var isSearchOpen = false;
 
@@ -176,7 +175,7 @@
         element.className = element.className.replace(className, '');
     };
 
-    polyfillQuery();
+    //polyfillQuery();
     mainNav();
     mainSubNav();
     mainSearch();
