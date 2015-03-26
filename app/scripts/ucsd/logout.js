d22c2ea36fbb02397fe65c48f6297b5abf7254bd
Jonathan Chang <jdkschang@gmail.com>
2015-03-26 10:10:59 -0700

    updated copyright.js and logout.js code to fit with new decorator 45

--- app/scripts/ucsd/logout.js
+++ app/scripts/ucsd/logout.js
@@ -3,11 +3,12 @@ function initLogout(logoutUrl) {
 	var url = "https://a4.ucsd.edu/tritON/resources/bugscript.jsp?target=https%3A%2F%2Fwww.ucsd.edu&jsoncallback=?";
 	$.getJSON(url, function(data) {
 		if (data.eduUcsdActLoggedin) {
-			var url = "<div id=\"tdr_login_content\">You are logged in | <a href=\"https://act.ucsd.edu/security/logout?url=";
+			var layoutLogin = '.layout-login';
+			var url = "<span class=\"login-content\">You are logged in | <a href=\"https://act.ucsd.edu/security/logout?url=";
 			url += logoutUrl;
 			url += "\">Log Out</a></div>";
-			$("div#tdr_login").empty();
-			$("div#tdr_login").append(url);
+			$(layoutLogin).empty();
+			$(layoutLogin).append(url);
 		}
 	});
 };
