60dd7b6396b40df6acf8ef4861634b500e59d5fb
Jonathan Chang <jdkschang@gmail.com>
2015-03-26 12:02:55 -0700

    changes to get layout-login / login and logout content working properly

--- app/scripts/ucsd/logout.js
+++ app/scripts/ucsd/logout.js
@@ -3,10 +3,10 @@ function initLogout(logoutUrl) {
 	var url = "https://a4.ucsd.edu/tritON/resources/bugscript.jsp?target=https%3A%2F%2Fwww.ucsd.edu&jsoncallback=?";
 	$.getJSON(url, function(data) {
 		if (data.eduUcsdActLoggedin) {
-			var layoutLogin = '.layout-login > .layout-container';
+			var layoutLogin = '.layout-login .layout-container';
 			var url = "<span class=\"login-content\">You are logged in | <a href=\"https://act.ucsd.edu/security/logout?url=";
 			url += logoutUrl;
-			url += "\">Log Out</a></div>";
+			url += "\">Log Out</a></span>";
 			$(layoutLogin).empty();
 			$(layoutLogin).append(url);
 		}
