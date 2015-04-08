cb5743f837e23f4e319bcc03e789f877187107a2
Jonathan Chang <jdkschang@gmail.com>
2015-04-08 09:24:29 -0700

    added modified addclass/removeclass to logout.js and css to go with the changes

--- app/scripts/ucsd/logout.js
+++ app/scripts/ucsd/logout.js
@@ -2,10 +2,11 @@
 function initLogout(logoutUrl) {
 	var url = "https://a4.ucsd.edu/tritON/resources/bugscript.jsp?target=https%3A%2F%2Fwww.ucsd.edu&jsoncallback=?";
 	$.getJSON(url, function(data) {
+		var loginHeader = $('.layout-header');
+		var loginStatus = 'isLoggedIn';
+
 		if (data.eduUcsdActLoggedin) {
-			var loginHeader = $('.layout-header')[0];
 			var layoutLogin = '.layout-login .layout-container';
-			var loginStatus = 'isLoggedin';
 			var url = "<span class=\"login-content\">You are logged in | <a href=\"https://act.ucsd.edu/security/logout?url=";
 
 			url += logoutUrl;
@@ -13,9 +14,9 @@ function initLogout(logoutUrl) {
 			$(layoutLogin).empty();
 			$(layoutLogin).append(url);
 
-			loginHeader.height(105);
+			loginHeader.addClass(loginStatus);
 		} else {
-
+			loginHeader.removeClass(loginStatus);
 		}
 	});
 };
