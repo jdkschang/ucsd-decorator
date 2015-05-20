b627a48dbaad68cf599612767c4116242f05da04
Jonathan Chang <jdkschang@gmail.com>
2015-05-20 09:10:00 -0700

    added homepage.html to useminPrepare

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function (grunt) {
             options: {
                 dest: '<%= config.dist %>'
             },
-            html: ['<%= config.app %>/homepage-wide.html']
+            html: ['<%= config.app %>/homepage-wide.html', '<%= config.app %>/homepage.html']
         },
 
         // Performs rewrites based on rev and the useminPrepare configuration
