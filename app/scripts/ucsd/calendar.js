4b1ca371b28fdc210de3405a381bb815788a5e07
Jonathan Chang <jdkschang@gmail.com>
2015-06-29 15:24:01 -0700

    a | title attribute to calendar json, a | manual triggers for popover

--- app/scripts/ucsd/calendar.js
+++ app/scripts/ucsd/calendar.js
@@ -7,17 +7,23 @@ $(document).ready( function() {
             //console.log("event name: " + calEvent.name);
             //console.log("event date: " + calEvent.date);
 
-            $(this).popover('show');
-
             //var output_url = "cal-output.html?id=";
             //window.location.href = output_url + calEvent.id;;
         },
         eventRender: function( event, element) {
+            var eventDate = formatTime(event.date)[0],
+                eventTime = formatTime(event.date)[1],
+                contentEvent = "Date: " + eventDate + "Time: " + eventTime,
+                output_url = "cal-output.html?"
+
+
+
             element.popover({
-                html: true,
+                html: false,
                 placement: 'top',
-                title: "testing title",
-                content: event.msg
+                trigger: 'click focus',
+                title: event.title,
+                content: contentEvent
             })
         },
         eventSources: [
@@ -27,4 +33,25 @@ $(document).ready( function() {
             }
         ]
     });
+
+    function formatTime( rawDate ) {
+        var dateTimeOutput = [];
+
+        dateTimeOutput.push( moment( rawDate ).format( "dddd, MMMM Do YYYY" ));
+        dateTimeOutput.push( moment( rawDate ).format( "h:mm a" ));
+
+        return dateTimeOutput;
+    }
+
+    /**
+     * manual triggers popover
+     *
+     * multiple trigger not working
+     */
+    var showPopover = function () {
+            $(this).popover('show');
+        },
+        hidePopover = function () {
+            $(this).popover('hide');
+        };
 });
