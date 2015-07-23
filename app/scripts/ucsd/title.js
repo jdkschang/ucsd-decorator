fb530339dd2b53af3997188d890438e08e61b61c
Jonathan Chang <jdkschang@gmail.com>
2015-07-23 13:44:15 -0700

    m | refactored check into fcn

--- app/scripts/ucsd/title.js
+++ app/scripts/ucsd/title.js
@@ -3,45 +3,52 @@
         titleShort = $(".title-header-short"),
         titleWidth = title[0].offsetWidth,
         logoWidth = 229,
-        titleOverflow = titleWidth + logoWidth + 1,
+        menuWidth = 62,
+        titleOverflow,
         titleWrapper;
 
     var removeLongTitle = function () {
-        title.toggle(false);
-        titleShort.toggle(true);
+        title.toggle( false );
+        titleShort.toggle( true );
     };
 
     var showLongTitle = function () {
-        title.toggle(true);
-        titleShort.toggle(false);
+        title.toggle( true );
+        titleShort.toggle( false );
+    };
+
+    var checkOverflow = function( titleOverflow, titleWrapper ) {
+        if ( titleOverflow > titleWrapper ) {
+            removeLongTitle();
+        } else {
+            showLongTitle();
+        }
     };
 
     var checkTitleOverflow = function () {
         // titleWrapper initialized here to dynamically
         titleWrapper = $(".layout-title .layout-container")[0].offsetWidth;
 
-        console.log(titleOverflow);
-        console.log(titleWrapper);
+        //console.log(titleOverflow);
+        //console.log(titleWrapper);
 
-        if (titleWrapper >= 960) {
-            if (titleOverflow > titleWrapper) {
-                console.log("titleOverflow is greater");
-                console.log("titleWrapper: " + titleWrapper + "   titleOverflow: " + titleOverflow);
-                removeLongTitle();
-            } else {
-                console.log("titleOverflow is NOT greater");
-                console.log("titleWrapper: " + titleWrapper + "   titleOverflow: " + titleOverflow);
-                showLongTitle();
-            }
+        if ( titleWrapper >= 960 ) {
+            titleOverflow =  titleWidth + logoWidth + 1;
+
+            checkOverflow( titleOverflow, titleWrapper);
+        } else if( titleWrapper > 768 ) {
+            titleOverflow = titleWidth + logoWidth + menuWidth + 1;
+
+            checkOverflow( titleOverflow, titleWrapper);
         }
     };
 
     // ToDo: function callback only registers twice
-    $(window).resize(function () {
+    $(window).resize( function () {
         checkTitleOverflow();
     });
 
-    $(window).ready(function () {
+    $(window).ready( function () {
         checkTitleOverflow();
     });
 })(document);
