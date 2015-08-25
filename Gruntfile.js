5380455b7c679fbb8057a31d7a726e4160ba6426
Jonathan Chang <jdkschang@gmail.com>
2015-08-25 14:27:24 -0700

    m | gruntfile.js jshint replaced by eslint in watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
             },
             js: {
                 files: ['<%= config.app %>/scripts/{,*/}*.js'],
-                tasks: ['jshint'],
+                tasks: ['eslint'],
                 options: {
                     livereload: true
                 }
@@ -132,18 +132,18 @@ module.exports = function (grunt) {
         },
 
         // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfile.js',
-                '<%= config.app %>/scripts/{,*/}*.js',
-                '!<%= config.app %>/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
-            ]
-        },
+        //jshint: {
+        //    options: {
+        //        jshintrc: '.jshintrc',
+        //        reporter: require('jshint-stylish')
+        //    },
+        //    all: [
+        //        'Gruntfile.js',
+        //        '<%= config.app %>/scripts/{,*/}*.js',
+        //        '!<%= config.app %>/scripts/vendor/*',
+        //        'test/spec/{,*/}*.js'
+        //    ]
+        //},
 
         // Mocha testing framework configuration options
         mocha: {
