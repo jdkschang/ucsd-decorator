5e406e64d37f85c241bf155ef70d6489af07d0c8
Jonathan Chang <jdkschang@gmail.com>
2015-09-02 13:45:48 -0700

    a | clean.js & imagemin.js for grunt; m | Gruntfile.js shifted use strict

---
+++ grunt-configs/imagemin.js
@@ -0,0 +1,21 @@
+// ================================================================================
+// GRUNT CONFIG: IMAGEMIN
+// Minifies images using imagemin.
+// grunt-contrib-imagemin
+// https://github.com/gruntjs/grunt-contrib-imagemin
+// ================================================================================
+
+module.exports = function(grunt) {
+    'use strict';
+    grunt.config('imagemin', {
+        options: {
+            optimizationLevel: 3
+        },
+        files: [{
+            expand: true,
+            cwd: 'src/',
+            src: ['images/**/*.{png,jpg,gif,svg}'],
+            dest: 'dist/'
+        }]
+    });
+};
